78750e0f9ca11332dba0f989c5b9c4adcc39c055
Konrad Brożyna <kondzio3002@gmail.com>
2022-10-15 23:25:55 +0200

    Add sendBooking Booking.js

--- src/js/settings.js
+++ src/js/settings.js
@@ -64,6 +64,12 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    floorPlan: '.floor-plan',
+    phone: '[name="phone"]',
+    address: '[name="address"]', 
+    form: '.booking-form',
+    starter: '[name="starter"]' ,
+    starterCheckbox: '.starter',
   },
   nav: {
     links: '.main-nav a',
@@ -85,6 +91,7 @@ export const classNames = {
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
     active: 'active',
